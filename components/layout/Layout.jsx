60e27fe69ea17d0cae79a5c6948cc4bb687006b1
Masoud <masoud.zeinalabedini@gmail.com>
2023-06-28 15:38:53 +0330

    Add context in helper

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import { BsFillMoonFill, BsMoon } from "react-icons/bs";
 
 export default function Layout() {
   return (
     <>
-    
+      <header>
+        <div>Tournament Bracket Generator</div>
+        <nav>
+          <Link href="/new">Create new Tournament</Link>
+          <Link href="/list">Tournaments List</Link>
+        </nav>
+        <div>
+          {darkMode ? <BsFillMoonFill /> : <BsMoon />}
+          <p>Dark Mode</p>
+        </div>
+      </header>
     </>
-  )
+  );
 }
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import { useState } from "react";
+import { Dark } from "@/helper/Contexts";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [darkMode, setDarkMode] = useState(true);
+  return;
+  <Dark.Provider value={{ darkMode, setDarkMode }}>
+    <Component {...pageProps} />
+  </Dark.Provider>;
 }
