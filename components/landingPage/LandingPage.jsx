c5142c9789369f2e38e6d3c63a8f38496cf871ae
Masoud <masoud.zeinalabedini@gmail.com>
2023-07-01 14:40:41 +0330

    Add firebase config

--- components/landingPage/LandingPage.jsx
+++ components/landingPage/LandingPage.jsx
@@ -1,5 +1,15 @@
-import React from "react";
+import Image from "next/image";
+import styles from "./LandingPageStyle.module.css";
 
 export default function LandingPage() {
-  return <div>LandingPage</div>;
+  return (
+    <div className={styles.container}>
+      <h1>
+        Generate Brackets
+        <br />
+        for All your Tournaments
+      </h1>
+      <Image src="/Tournament.png" alt="globe" width="300" height="300" />
+    </div>
+  );
 }
---
+++ config/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import {auth} from "firebase"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAsKX77w5FpwlGuKjuC3_pG8zGNLKVpDi4",
+  authDomain: "tournament-brackets-1301f.firebaseapp.com",
+  projectId: "tournament-brackets-1301f",
+  storageBucket: "tournament-brackets-1301f.appspot.com",
+  messagingSenderId: "544158190540",
+  appId: "1:544158190540:web:b0a9edf2bf6b5ba73fab69"
+};
+
+
+const app = initializeApp(firebaseConfig);
