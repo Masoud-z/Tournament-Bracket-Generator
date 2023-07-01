17618a994c23db0e6e464eedc5e7e9f4ff44ad7c
Masoud <masoud.zeinalabedini@gmail.com>
2023-07-01 15:29:46 +0330

    Add Signup page

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth"
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAsKX77w5FpwlGuKjuC3_pG8zGNLKVpDi4",
@@ -7,9 +7,8 @@ const firebaseConfig = {
   projectId: "tournament-brackets-1301f",
   storageBucket: "tournament-brackets-1301f.appspot.com",
   messagingSenderId: "544158190540",
-  appId: "1:544158190540:web:b0a9edf2bf6b5ba73fab69"
+  appId: "1:544158190540:web:b0a9edf2bf6b5ba73fab69",
 };
 
-
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+export const auth = getAuth(app);
