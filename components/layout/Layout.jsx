d7f6c6ac6d79afe78df3db898940b333f3009ae7
Masoud <masoud.zeinalabedini@gmail.com>
2023-07-01 12:03:28 +0330

    Add Material UI to the project

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,21 +1,24 @@
+import { useContext } from "react";
 import Link from "next/link";
-import React from "react";
 import { BsFillMoonFill, BsMoon } from "react-icons/bs";
+import { Dark } from "@/helper/Contexts";
 
-export default function Layout() {
+export default function Layout(props) {
+  const { darkMode, setDarkMode } = useContext(Dark);
   return (
     <>
-      <header>
-        <div>Tournament Bracket Generator</div>
+      <header className={darkMode ? "dark" : "light"}>
+        <div className="logo">Tournament Bracket Generator</div>
         <nav>
           <Link href="/new">Create new Tournament</Link>
           <Link href="/list">Tournaments List</Link>
         </nav>
-        <div>
+        <div onClick={() => setDarkMode((perv) => !perv)}>
           {darkMode ? <BsFillMoonFill /> : <BsMoon />}
           <p>Dark Mode</p>
         </div>
       </header>
+      <main>{props.children}</main>
     </>
   );
 }
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import { Dark } from "@/helper/Contexts";
+import Layout from "@/components/layout/Layout";
 
 export default function App({ Component, pageProps }) {
   const [darkMode, setDarkMode] = useState(true);
-  return;
-  <Dark.Provider value={{ darkMode, setDarkMode }}>
-    <Component {...pageProps} />
-  </Dark.Provider>;
+  return (
+    <Dark.Provider value={{ darkMode, setDarkMode }}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Dark.Provider>
+  );
 }
