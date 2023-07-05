7e2bf8000049e15179e1713eb1a7510682287720
Masoud <masoud.zeinalabedini@gmail.com>
2023-07-05 11:55:32 +0330

    Update Layout.jsx

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { signOut, onAuthStateChanged } from "firebase/auth";
